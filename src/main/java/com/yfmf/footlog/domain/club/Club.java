c33e957e3b2f2c403585d18d476f4e0f8eea15d8
YachaTree <dnftksghkfkd@icloud.com>
2024-08-10 00:51:41 +0900

    Feat: 구단 entity 작성

---
+++ src/main/java/com/yfmf/footlog/domain/club/Club.java
@@ -0,0 +1,72 @@
+package com.yfmf.footlog.domain.club;
+
+import com.example.subjectjpa.user.User;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "tbl_club")
+public class Club {
+
+
+    @Id
+    @Column(name = "CLUB_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long clubId;
+
+    @ManyToOne
+    @JoinColumn(name = "CLUB_OWNER")
+    private User clubOwner;  //구단주
+
+    @Column(name = "CLUB_NAME")
+    private String clubName;  //구단이름
+
+    @Column(name = "CLUB_INTRODUCTION")
+    private String clubIntroduction;  //구단소개
+
+    @Column(name = "CLUB_CODE")
+    private String clubCode;  //구단코드
+
+    @Column(name = "EROLL_DATE")
+    private LocalDateTime erollDate;  //구단등록일
+
+    @Column(name = "PEAK_HOURS")
+    @Enumerated(EnumType.STRING)
+    private PeakHours peakHours;  //구단활동시간대 아침,찾,저녁,심야
+
+    @Column(name = "PEAK_DAYS")
+    @Enumerated(EnumType.STRING)
+    private List<PeakDays> peakDays;  // 월~일
+
+    public Club() {
+    }
+
+    public Club(User clubOwner,
+                String clubName, String clubIntroduction,
+                String clubCode, LocalDateTime erollDate,
+                PeakHours peakHours, List<PeakDays> peakDays) {
+        this.clubOwner = clubOwner;
+        this.clubName = clubName;
+        this.clubIntroduction = clubIntroduction;
+        this.clubCode = clubCode;
+        this.erollDate = erollDate;
+        this.peakHours = peakHours;
+        this.peakDays = peakDays;
+    }
+
+    @Override
+    public String toString() {
+        return "Club{" +
+                "clubId=" + clubId +
+                ", clubOwner=" + clubOwner +
+                ", clubName='" + clubName + '\'' +
+                ", clubIntroduction='" + clubIntroduction + '\'' +
+                ", clubCode='" + clubCode + '\'' +
+                ", erollDate=" + erollDate +
+                ", peakHours=" + peakHours +
+                ", peakDays=" + peakDays +
+                '}';
+    }
+}
