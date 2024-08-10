a2fa9dfdb9de82a88baf5f180e31dda00a0f4c28
junsyun22 <junsyun22@gmail.com>
2024-08-10 16:06:49 +0900

    Fix: Guest Entity에 @Column 명시

--- src/main/java/com/yfmf/footlog/guest/Entity/Guest.java
+++ src/main/java/com/yfmf/footlog/guest/Entity/Guest.java
@@ -1,5 +1,7 @@
 package com.yfmf.footlog.guest.Entity;
 
+import com.yfmf.footlog.enums.MainFoot;
+import com.yfmf.footlog.enums.Position;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
@@ -11,18 +13,22 @@ public class Guest {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "createdAt")
     private LocalDateTime createdAt;
 
+
     @Enumerated(EnumType.STRING)
+    @Column(name = "mainFoot")
     private MainFoot mainFoot;
 
     @Enumerated(EnumType.STRING)
+    @Column(name = "position")
     private Position position;
 
-    private String contactInfo;
-
+    @Column(name = "isAvailable")
     private boolean isAvailable; // 현재 활동 가능 여부
 
     // Guest 관련 도메인 로직
@@ -33,7 +39,5 @@ public class Guest {
     public void becomeUnavailable() {
         this.isAvailable = false;
     }
-
-
 
 }
