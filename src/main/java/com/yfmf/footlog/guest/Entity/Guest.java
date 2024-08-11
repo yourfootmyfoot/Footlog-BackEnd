1a1c5c91b95b08594f2bbcf4f471908f8b402f68
junsyun22 <junsyun22@gmail.com>
2024-08-11 14:23:41 +0900

    Feat: Guest 생성자, getter setter 추가 (불변성을 위해 setter보단builder pattern 쓰는게 좋을 수도)

--- src/main/java/com/yfmf/footlog/guest/Entity/Guest.java
+++ src/main/java/com/yfmf/footlog/guest/Entity/Guest.java
@@ -31,6 +31,66 @@ public class Guest {
     @Column(name = "isAvailable")
     private boolean isAvailable; // 현재 활동 가능 여부
 
+    public Guest() {
+    }
+
+
+    public Guest(String name, LocalDateTime createdAt, MainFoot mainFoot, Position position, boolean isAvailable) {
+        this.name = name;
+        this.createdAt = createdAt;
+        this.mainFoot = mainFoot;
+        this.position = position;
+        this.isAvailable = isAvailable;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setMainFoot(MainFoot mainFoot) {
+        this.mainFoot = mainFoot;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public MainFoot getMainFoot() {
+        return mainFoot;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
     // Guest 관련 도메인 로직
     public void becomeAvailable() {
         this.isAvailable = true;
