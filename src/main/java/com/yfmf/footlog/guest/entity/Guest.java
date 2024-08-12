211a02cfcfb0f1f263c679cf6a4cc0da4f39361a
junsyun22 <junsyun22@gmail.com>
2024-08-13 00:28:28 +0900

    Fix: Entity 실수로 인한 삭제. 재업로드

--- src/main/java/com/yfmf/footlog/guest/entity/Guest.java
+++ src/main/java/com/yfmf/footlog/guest/entity/Guest.java
@@ -1,1 +1,35 @@
-:
+package com.yfmf.footlog.guest.entity;
+
+import com.yfmf.footlog.enums.MainFoot;
+import com.yfmf.footlog.enums.Position;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
+@ToString
+public class Guest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private LocalDateTime createdAt;
+
+    @Enumerated(EnumType.STRING)
+    private MainFoot mainFoot;
+
+    @Enumerated(EnumType.STRING)
+    private Position position;
+
+    private Boolean isAvailable; // 현재 활동 가능 여부
+
+}
