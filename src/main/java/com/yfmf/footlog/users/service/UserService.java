c0979557ca00898f04b8572ec68c6039b247499c
Cheong Ye Jun <jjyejun@naver.com>
2024-08-16 18:34:00 +0900

    Feat: UserService CUD 구현

--- src/main/java/com/yfmf/footlog/users/service/UserService.java
+++ src/main/java/com/yfmf/footlog/users/service/UserService.java
@@ -1,5 +1,8 @@
 package com.yfmf.footlog.users.service;
 
+import com.yfmf.footlog.users.dto.UserSaveRequestDto;
+import com.yfmf.footlog.users.dto.UserUpdateRequestDto;
+import com.yfmf.footlog.users.entity.User;
 import com.yfmf.footlog.users.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,5 +15,25 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    public Long save(UserSaveRequestDto requestDto) {
+
+        return userRepository.save(requestDto.toEntity()).getUserId();
+    }
+
+    public Long update(Long id, UserUpdateRequestDto requestDto) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("해당 유저가 없습니다. id=" + id));
+
+        user.update(requestDto);
+
+        return id;
+    }
+
+    public void delete(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("해당 유저가 없습니다. id=" + id));
+
+        userRepository.delete(user);
+    }
 
 }
