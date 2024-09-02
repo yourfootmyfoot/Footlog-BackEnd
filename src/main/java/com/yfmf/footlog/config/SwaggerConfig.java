5ac8cc9fc88a1991b4b4ea58bf2a404043b28d3b
YachaTree <dnftksghkfkd@icloud.com>
2024-09-03 03:25:37 +0900

    feat: build.gradle swagger 추가 및 config 디렉토리 추가

---
+++ src/main/java/com/yfmf/footlog/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.yfmf.footlog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    /*swagger JWT config*/
+    @Bean
+    public OpenAPI api() {
+        SecurityScheme apiKey = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization")
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("Bearer Token");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
+                .addSecurityItem(securityRequirement);
+    }
+}
