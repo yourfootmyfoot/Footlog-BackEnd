1c84d2c8b53d9126254f099713df2c7c2a87cd99
LooanCheong <jjyejun@naver.com>
2024-09-07 21:58:14 +0900

    Refacotr: 토큰 필터 로직 변경

--- src/main/java/com/yfmf/footlog/login/jwt/filter/JwtFilter.java
+++ src/main/java/com/yfmf/footlog/login/jwt/filter/JwtFilter.java
@@ -3,9 +3,9 @@ package com.yfmf.footlog.login.jwt.filter;
 import com.yfmf.footlog.login.dto.CustomOAuth2User;
 import com.yfmf.footlog.login.jwt.util.JwtUtil;
 import com.yfmf.footlog.users.dto.UserOAuth2Dto;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +16,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -27,55 +28,48 @@ public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        String requestUri = request.getRequestURI();
+        // 헤더에서 access키에 담긴 토큰을 꺼냄
+        String accessToken = request.getHeader("access");
 
-        if (requestUri.matches("^\\/login(?:\\/.*)?$")) {
-
-            filterChain.doFilter(request, response);
-            return;
-        }
-        if (requestUri.matches("^\\/oauth2(?:\\/.*)?$")) {
+// 토큰이 없다면 다음 필터로 넘김
+        if (accessToken == null) {
 
             filterChain.doFilter(request, response);
-            return;
-        }
 
-        // 쿠키들을 불러온 뒤 Authorization Key에 담긴 쿠키를 찾음
-        String authorization = null;
-        Cookie[] cookies = request.getCookies();
-
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("Authorization")) {
-
-                authorization = cookie.getValue();
-            }
-        }
-
-        // Authorization 헤더 검증
-        if (authorization == null) {
-
-            log.info("Authorization cookie not found");
-            filterChain.doFilter(request, response);
-
-            //조건이 해당되면 메서드 종료
             return;
         }
 
-        // 토큰
-        String token = authorization;
+// 토큰 만료 여부 확인, 만료시 다음 필터로 넘기지 않음
+        try {
+            jwtUtil.isExpired(accessToken);
+        } catch (ExpiredJwtException e) {
 
-        // 토큰 소멸 시간 검증
-        if (jwtUtil.isExpired(token)) {
+            //response body
+            PrintWriter writer = response.getWriter();
+            writer.print("access token expired");
 
-            log.info("token expired");
-            filterChain.doFilter(request, response);
+            //response status code
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
 
+// 토큰이 access인지 확인 (발급시 페이로드에 명시)
+        String category = jwtUtil.getCategory(accessToken);
+
+        if (!category.equals("access")) {
+
+            //response body
+            PrintWriter writer = response.getWriter();
+            writer.print("invalid access token");
+
+            //response status code
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return;
         }
 
         // 토큰에서 username과 role 획득
-        String username = jwtUtil.getUsername(token);
-        String role = jwtUtil.getRole(token);
+        String username = jwtUtil.getUsername(accessToken);
+        String role = jwtUtil.getRole(accessToken);
 
         // userDto를 생성해서 값을 세팅
         UserOAuth2Dto userDto = new UserOAuth2Dto();
