9a8f2cba889d41018b2b1c9ac6fdd7745aca291f
Cheong Ye Jun <jjyejun@naver.com>
2024-09-09 15:28:06 +0900

    Refactor: 로그인 컨트롤러 어노테이션 변경

--- src/main/java/com/yfmf/footlog/login/LoginController.java
+++ src/main/java/com/yfmf/footlog/login/LoginController.java
@@ -4,14 +4,14 @@ import com.yfmf.footlog.login.service.LoginService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 @RequestMapping("/login")
 public class LoginController {
 
