fdc9a1c96801eb48943151208f63c8a413a23e25
MungChi <2ee42a45@gmail.com>
2024-09-24 03:50:12 +0900

    Refactory : match 테이블 리펙토링

--- src/main/java/com/yfmf/footlog/domain/match/entity/Match.java
+++ src/main/java/com/yfmf/footlog/domain/match/entity/Match.java
@@ -20,33 +20,33 @@ public class Match extends BaseTimeEntity {
     // 경기 아이디
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT 사용
-    @Column(nullable = false)
+    @Column(name = "match_id", nullable = false)
     private Long matchId;
 
     // 경기 생성 유저 아이디
-    @Column(nullable = false)
+    @Column(name="match_enroll_user_id", nullable = false)
     private Long matchEnrollUserId;
 
     // 경기 신청 유저 아이디
-    @Column(nullable = false)
+    @Column(name="match_apply_user_id", nullable = false)
     private Long matchApplyUserId;
 
     // 내 구단 - 구단 라인업, 매치 생성 매니저, 구단 이름, 구단 로고, 구단
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "my_club_id", referencedColumnName = "CLUB_ID", nullable = false)
     private Club myClub;
 
     // 상대 구단 - 구단 이름, 구단 로고
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "enemy_club_id", referencedColumnName = "CLUB_ID", nullable = false)
     private Club enemyClub;
 
     // 매치 대표사진
-    @Column
+    @Column(name="match_photo")
     private String matchPhoto;
 
     // 매치 설명
-    @Column(length = 500)
+    @Column(name="match_introduce",length = 500)
     private String matchIntroduce;
 
     // 매치 일정
@@ -54,21 +54,21 @@ public class Match extends BaseTimeEntity {
     private MatchSchedule matchSchedule;
 
     // 매치 인원
-    @Column(nullable = false)
+    @Column(name="match_player_quantity",nullable = false)
     @Enumerated(EnumType.STRING)
     private MatchPlayerQuantity matchPlayerQuantity;
 
     // 쿼터 수
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(name="quarter_quantity",nullable = false)
     private QuarterQuantity quarterQuantity;
 
     // 구장 위치
-    @Column(nullable = false)
+    @Column(name="field_location",nullable = false)
     private String fieldLocation;
 
     // 매치 비용
-    @Column
+    @Column(name="match_cost")
     private Integer matchCost;
 
     // 선출 여부 & 선출 수
@@ -76,17 +76,17 @@ public class Match extends BaseTimeEntity {
     private Pro pro;
 
     // 실력수치
-    @Column(nullable = false)
+    @Column(name="club_level",nullable = false)
     @Enumerated(EnumType.STRING)
     private ClubLevel clubLevel;
 
     // 성별 - male, female, mix
-    @Column(nullable = false)
+    @Column(name="match_gender",nullable = false)
     @Enumerated(EnumType.STRING)
     private MatchGender matchGender;
 
     // 경기 상태
-    @Column(nullable = false)
+    @Column(name="match_status",nullable = false)
     @Enumerated(EnumType.STRING)
     private MatchStatus matchStatus;
 
