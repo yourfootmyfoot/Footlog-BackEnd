0a281b7b2b8fdf9759fb2484041320add97c13a1
Cheong Ye Jun <jjyejun@naver.com>
2024-10-16 18:19:48 +0900

    Feat: 1대1 문의 응답용 DTO 생성, Vaild 조건 추가

---
+++ src/main/java/com/yfmf/footlog/domain/ask/dto/AskCreateResponseDto.java
@@ -0,0 +1,31 @@
+package com.yfmf.footlog.domain.ask.dto;
+
+import com.yfmf.footlog.domain.ask.enums.AskCategory;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AskCreateResponseDto {
+
+    @NotBlank(message = "문의 ID는 필수입니다.")
+    private Long askId;
+
+    @NotBlank(message = "userId는 필수입니다.")
+    private Long userId;
+
+    @NotBlank(message = "문의 카테고리는 필수입니다.")
+    private AskCategory category;
+
+    @NotBlank(message = "문의 제목은 필수입니다.")
+    @Size(min = 2, max = 40, message = "문의 제목은 2 ~ 40 글자입니다.")
+    private String title;
+
+    @NotBlank(message = "문의 내용은 필수입니다.")
+    @Size(min = 5, message = "문의 내용은 5글자 이상입니다.")
+    private String content;
+}
