d5ce0e43e7db0895e618e533ea43fcddb340ddf8
MungChi <2ee42a45@gmail.com>
2024-11-05 13:45:02 +0900

    Fix : ddl, 3002 포트 추가

--- src/main/java/com/yfmf/footlog/config/WebConfig.java
+++ src/main/java/com/yfmf/footlog/config/WebConfig.java
@@ -11,10 +11,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")  // React 앱이 돌아가는 도메인
+                .allowedOrigins("https://localhost:3002")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true)  // 쿠키를 포함하는 요청 허용
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization");
+
     }
 }
 
