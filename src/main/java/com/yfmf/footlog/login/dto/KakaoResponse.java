f0c9ed12846569537ebb7952da9b5b26758e06f5
YachaTree <dnftksghkfkd@icloud.com>
2024-11-19 15:07:42 +0900

    Fix: login 디렉토리 주석 처리

--- src/main/java/com/yfmf/footlog/login/dto/KakaoResponse.java
+++ src/main/java/com/yfmf/footlog/login/dto/KakaoResponse.java
@@ -1,32 +1,32 @@
-package com.yfmf.footlog.login.dto;
-
-import java.util.Map;
-
-public class KakaoResponse implements OAuth2Response {
-
-    private final Map<String, Object> attribute;
-
-    public KakaoResponse(Map<String, Object> attribute) {
-        this.attribute = (Map<String, Object>) attribute.get("response");
-    }
-
-    @Override
-    public String getProvider() {
-        return "kakao";
-    }
-
-    @Override
-    public String getProviderId() {
-        return attribute.get("id").toString();
-    }
-
-    @Override
-    public String getEmail() {
-        return attribute.get("account_email").toString();
-    }
-
-    @Override
-    public String getName() {
-        return attribute.get("profile_nickname").toString();
-    }
-}
+//package com.yfmf.footlog.login.dto;
+//
+//import java.util.Map;
+//
+//public class KakaoResponse implements OAuth2Response {
+//
+//    private final Map<String, Object> attribute;
+//
+//    public KakaoResponse(Map<String, Object> attribute) {
+//        this.attribute = (Map<String, Object>) attribute.get("response");
+//    }
+//
+//    @Override
+//    public String getProvider() {
+//        return "kakao";
+//    }
+//
+//    @Override
+//    public String getProviderId() {
+//        return attribute.get("id").toString();
+//    }
+//
+//    @Override
+//    public String getEmail() {
+//        return attribute.get("account_email").toString();
+//    }
+//
+//    @Override
+//    public String getName() {
+//        return attribute.get("profile_nickname").toString();
+//    }
+//}
