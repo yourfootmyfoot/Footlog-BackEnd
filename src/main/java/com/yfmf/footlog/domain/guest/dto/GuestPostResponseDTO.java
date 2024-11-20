0e0b942737f6d08aa33f3ca12ce6d21dc7593167
YachaTree <dnftksghkfkd@icloud.com>
2024-11-20 12:55:40 +0900

    Feat: 용병 dto 스웨거 명세 상세화

--- src/main/java/com/yfmf/footlog/domain/guest/dto/GuestPostResponseDTO.java
+++ src/main/java/com/yfmf/footlog/domain/guest/dto/GuestPostResponseDTO.java
@@ -5,6 +5,7 @@ import com.yfmf.footlog.domain.guest.enums.PostStatus;
 import com.yfmf.footlog.domain.member.domain.Member;
 import com.yfmf.footlog.domain.member.dto.MemberSimpleDTO;
 import com.yfmf.footlog.domain.member.enums.Position;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,16 +17,37 @@ import java.time.LocalTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "용병 지원글 응답 데이터")
 public class GuestPostResponseDTO {
+
+    @Schema(description = "지원글 ID", example = "1")
     private Long id;
+
+    @Schema(description = "작성자 간단 정보")
     private MemberSimpleDTO member;
+
+    @Schema(description = "활동 가능 날짜", example = "2024-11-20")
     private LocalDate availableDate;
+
+    @Schema(description = "활동 가능 시작 시간", example = "14:00")
     private LocalTime availableStartTime;
+
+    @Schema(description = "활동 가능 종료 시간", example = "16:00")
     private LocalTime availableEndTime;
+
+    @Schema(description = "선호 포지션", example = "MIDFIELDER")
     private Position preferredPosition;
+
+    @Schema(description = "활동 지역", example = "서울시 강남구")
     private String location;
+
+    @Schema(description = "추가 설명", example = "적극적인 플레이를 선호합니다.")
     private String description;
+
+    @Schema(description = "지원글 상태", example = "ACTIVE")
     private PostStatus status;
+
+    @Schema(description = "작성 날짜 및 시간", example = "2024-11-19T14:00:00")
     private LocalDateTime createdAt;
 
     public GuestPostResponseDTO(GuestPost post, Member member) {
--- src/main/java/com/yfmf/footlog/domain/guest/dto/GuestRecruitmentDetailDTO.java
+++ src/main/java/com/yfmf/footlog/domain/guest/dto/GuestRecruitmentDetailDTO.java
@@ -2,6 +2,7 @@ package com.yfmf.footlog.domain.guest.dto;
 
 import com.yfmf.footlog.domain.club.entity.Club;
 import com.yfmf.footlog.domain.guest.entity.GuestRecruitment;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,8 +12,11 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "용병 모집 상세 정보 DTO")
 public class GuestRecruitmentDetailDTO extends GuestRecruitmentResponseDTO {
-    private List<GuestApplicationResponseDTO> applications;  // 신청자 목록
+
+    @Schema(description = "모집글에 대한 신청자 목록", required = true)
+    private List<GuestApplicationResponseDTO> applications;
 
     public GuestRecruitmentDetailDTO(GuestRecruitment recruitment, Club club,
                                      List<GuestApplicationResponseDTO> applications) {
