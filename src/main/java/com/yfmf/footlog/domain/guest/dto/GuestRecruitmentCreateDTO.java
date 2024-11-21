3d2eaa007701dfc06e87333fe16632911c28a98b
YachaTree <dnftksghkfkd@icloud.com>
2024-11-21 19:30:02 +0900

    Fix: 용병 모집글 dto 수정

--- src/main/java/com/yfmf/footlog/domain/guest/dto/GuestRecruitmentCreateDTO.java
+++ src/main/java/com/yfmf/footlog/domain/guest/dto/GuestRecruitmentCreateDTO.java
@@ -25,10 +25,6 @@ public class GuestRecruitmentCreateDTO {
     @NotNull(message = "모집글 제목을 입력해주세요.")
     private String title;
 
-    @Schema(description = "작성자 닉네임", example = "송호진", required = true)
-    @NotNull(message = "작성자 닉네임를 입력해주세요.")
-    private Long userId;
-
     @Schema(description = "구단 ID", example = "1", required = true)
     @NotNull(message = "구단 ID를 입력해주세요.")
     private Long clubId;
